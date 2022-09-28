ce29b8348ed9b914d3add2d2bd4788e6953fe49d
Heidelbergg <ammar260813@gmail.com>
2022-09-28 23:23:33 +0200

    Create/Edit event UI

---
+++ lib/event/event_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventDetailsScreen extends StatefulWidget {
+  const EventDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EventDetailsScreen> createState() => _EventDetailsScreenState();
+}
+
+class _EventDetailsScreenState extends State<EventDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
