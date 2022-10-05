5209b146e37672af071a74e4f0a3d570bd8bc09c
Heidelbergg <ammar260813@gmail.com>
2022-10-05 20:46:14 +0200

    Show events user has created/participated in
    Show relevant button depending on current status of event.
    
    Show checkmark if user is a part of an event.

--- lib/assets/event_card.dart
+++ lib/assets/event_card.dart
@@ -5,6 +5,7 @@ class EventCard extends StatelessWidget {
   final String day;
   final String time;
   final Icon icon;
+  final bool check;
   final Function() onPressed;
 
   const EventCard(
@@ -12,6 +13,7 @@ class EventCard extends StatelessWidget {
         required this.day,
         required this.icon,
         required this.time,
+        required this.check,
         required this.onPressed,
         Key? key})
       : super(key: key);
@@ -74,6 +76,63 @@ class EventCard extends StatelessWidget {
                         padding: EdgeInsets.only(left: 10, top: 10),
                         child: icon,),
                   ],
+                ),
+              ],
+            ),
+            const Spacer(),
+           check? Container(
+              padding: EdgeInsets.only(right: 20),
+                child: Icon(Icons.check_circle, color: Colors.green, size: 28,)) : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EventHistoryCard extends StatelessWidget {
+  final Function() onPressed;
+  final String text;
+
+  const EventHistoryCard(
+      {required this.onPressed,
+        required this.text,
+        Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 75,
+        padding: const EdgeInsets.only(top: 5, bottom: 5),
+        margin: const EdgeInsets.only(bottom: 10, right: 20, left: 15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(17.5),
+          boxShadow: [
+            BoxShadow(
+                offset: const Offset(5, 5),
+                blurRadius: 15,
+                color: Colors.grey.withOpacity(.5)),
+          ],
+        ),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 15, top: 20, bottom: 5),
+                  child: Text('Event ID:  ${text}',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      )),
                 ),
               ],
             ),
